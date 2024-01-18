8384cfbe9caa47ffb02a85dab8352eafd3fc5f11
Juliana Myaki <j.myaki@gmail.com>
2024-01-17 21:01:21 -0300

    🚧 no layout, but gets json information

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+//import { example } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
 
-import data from './data/dataset.js';
+import data from "./data/dataset.js";
 
-console.log(example, renderItems(data), data);
+console.log(data);
+renderItems(data);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,27 @@
-export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+export const renderItems = (plants) => {
+  const plantsContainer = document.querySelector(".plants-container");
+
+  console.log("aquiiii");
+
+  if (plants.length > 0) {
+    plants.forEach((plant) => {
+      const dlPlant = createElement("dl", plantsContainer);
+      const dtNome = createElement("dt", dlPlant);
+      dtNome.textContent = "Nome";
+      const ddNome = createElement("dd", dlPlant);
+      ddNome.textContent = plant.name;
+    });
+  }
 };
 
+// createElement helper function
+function createElement(type, parent, classList) {
+  const element = document.createElement(type);
+  if (classList !== undefined) {
+    typeof classList === "object" && classList.length > 0
+      ? element.classList.add(...classList)
+      : (element.className = classList);
+  }
+  parent.append(element);
+  return element;
+}
