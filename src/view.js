f08a936c8c068700679e9f2ef51b3c841333779a
Juliana Myaki <j.myaki@gmail.com>
2024-01-22 18:41:56 -0300

    💥 broken layout, but using ul > li structure

--- src/view.js
+++ src/view.js
@@ -2,17 +2,17 @@ export const renderItems = (plants) => {
   const elements = plants.map(
     (
       plant
-    ) => `<li data-id="${plant.id}" itemscope itemtype="https://schema.org/Thing">
+    ) => `<li class="card" data-id="${plant.id}" itemscope itemtype="https://schema.org/Thing">
                   <picture class="img-overflow">
                     <img itemprop="image" src="./static/${plant.id}.jpg" />
                     <figcaption>${plant.name}</figcaption>
                   </picture>
-                  <h3 itemprop="name">${plant.name}</h3>
-                  <p itemprop="description">
+                  <dd itemprop="name">${plant.name}</dd>
+                  <dd itemprop="description">
                   ${plant.shortDescription}
-                  </p>
+                  </dd>
                 </li>
   `
   );
-  return `<ul class="card">${elements.join("")}</ul>`;
+  return `<ul class="plants-flex">${elements.join("")}</ul>`;
 };
