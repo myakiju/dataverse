f6238d905100e56c754e6c206de4ac3f034640e5
Juliana Myaki <j.myaki@gmail.com>
2024-02-02 14:36:37 -0300

    ✅ Add unit tests

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,18 +1,53 @@
-import { example, anotherExample } from '../src/dataFunctions.js';
-import { data as fakeData } from './data.js';
+import { filterBy, sortData, computeStats } from "../src/dataFunctions.js";
+import { data as fakeData } from "./data.js";
 
-console.log(fakeData);
+describe("filterData test - valida busca por um campo específico", () => {
+  it('A lista de plantas exibe somente as plantas com o sunExposure = "Sol pleno" ', () => {
+    const filterField = "sunExposure";
+    const value = "Sol pleno";
 
-describe('example', () => {
+    const result = filterBy(fakeData, filterField, value);
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+    expect(result).toHaveLength(2);
+    expect(
+      result.every((plant) => plant.facts[filterField].includes(value))
+    ).toBe(true);
+  });
+
+  it("Não exibe nada quando o valor filtrado não existir", () => {
+    const filterField = "sunExposure";
+    const value = "Atmosfera de Marte";
+
+    const result = filterBy(fakeData, filterField, value);
+
+    expect(result).toHaveLength(0);
   });
 });
 
-describe('anotherExample', () => {
+describe("sortDat test - Valida ordenações", () => {
+  const getNames = (plants) => plants.map((plant) => plant.name);
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it("Deve retornar uma lista ordenada por nome em ordem crescente", () => {
+    const sortedData = sortData("asc", [...fakeData]);
+    const namesOrdered = getNames(sortedData);
+    const expectedOrder = namesOrdered
+      .slice()
+      .sort((a, b) => a.localeCompare(b));
+    expect(namesOrdered).toEqual(expectedOrder);
+  });
+
+  it("Deve retornar uma lista ordenada por nome em ordem decrescente", () => {
+    const sortedData = sortData("desc", [...fakeData]);
+    const namesOrdered = getNames(sortedData);
+    const expectedOrder = namesOrdered
+      .slice()
+      .sort((a, b) => a.localeCompare(b) * -1);
+    expect(namesOrdered).toEqual(expectedOrder);
+  });
+});
+
+describe("computeStats test", () => {
+  it("A média de tempo de rega de todas as plantas", () => {
+    expect(computeStats(fakeData)).toBe(12.25);
   });
 });
